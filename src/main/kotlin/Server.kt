MrMinemeet/KoChat

commit 59d210b36ad4962b6b05a6489b871a62a6130636

Code cleanup and Document Comments

src/main/kotlin/Server.kt
@@ -3,32 +3,35 @@ import java.net.Socket
 import java.util.*
 import kotlin.concurrent.thread
 
-fun main() {
-	Server(PORT).listen()
-}
-
 class Server(private val port: Int) {
 	private val server = ServerSocket(port)
 
 	object SServer {
-		val clientList = mutableListOf<ClientData>()
+		// List of all connected clients
+		val connectedClients = mutableListOf<ClientData>()
 
-
-		fun distributeMessages(msg: Message) {
-			clientList
-				.filterNot { it == msg.sender }
+		/**
+		 * Sends a message to all clients except the sender
+		 * @param message The message to send
+		 */
+		fun distributeMessages(message: Message) {
+			connectedClients
+				.filterNot { it == message.sender }
 				.forEach { (_, socket) ->
-					sendMessageToSocket(socket, msg.toString())
+					sendMessageToSocket(socket, message.toString())
 				}
 		}
 	}
 
+	/**
+	 * Listens for incoming client connections
+	 * Starts up a new thread for each client
+	 */
 	fun listen() {
 		println("Server listening on port $port")
 		while (true) {
-			val socket = server.accept()
 			thread {
-				ClientHandler(socket).run()
+				ClientHandler(server.accept()).run()
 			}
 		}
 	}
@@ -36,6 +39,9 @@ class Server(private val port: Int) {
 	class ClientHandler(private val client: Socket) {
 		private val reader = Scanner(client.getInputStream())
 
+		/**
+		 * Handles incoming messages from a client
+		 */
 		fun run() {
 			// First message of a client is the name
 			val senderName: String = try {
@@ -46,7 +52,7 @@ class Server(private val port: Int) {
 				return
 			}
 			val sender = ClientData(senderName, client)
-			SServer.clientList.add(sender)
+			SServer.connectedClients.add(sender)
 			println("User '${sender}' connected")
 			while (true) {
 				try {
@@ -66,21 +72,33 @@ class Server(private val port: Int) {
 			}
 		}
 
+		/**
+		 * Disconnects a client
+		 */
 		private fun disconnect(sender: ClientData) {
 			client.close()
-			SServer.clientList.remove(sender)
+			SServer.connectedClients.remove(sender)
 			println("User '$sender' disconnected")
 		}
 	}
 }
 
+/**
+ * Message from a client with content.
+ * @param sender The client that sent the message
+ * @param content The content of the message
+ */
 data class Message(val sender: ClientData, val content: String) {
-
 	override fun toString(): String {
 		return "${sender.name}: $content"
 	}
 }
 
+/**
+ * Data class for a client
+ * @param name The name of the client
+ * @param client The socket of the client
+ */
 data class ClientData(val name: String, val client: Socket) {
 	override fun toString(): String {
 		return "$name <${client.inetAddress.hostAddress}:${client.port}>"

.gitignore
@@ -42,3 +42,4 @@ build/
 
 ## Builds ##
 out/
+bin/

src/main/kotlin/Client.kt
@@ -5,18 +5,6 @@ import java.nio.charset.StandardCharsets
 import java.util.*
 import kotlin.concurrent.thread
 
-fun main() {
-	println("Enter a address to connect to: ")
-	val address = readln()
-
-	println("Enter your username: ")
-	try {
-		Client(readln(), host=address).run()
-	} catch (e: ConnectException) {
-		println("Failed to connect to server with address '$address!")
-	}
-}
-
 class Client(username: String, host: String = "127.0.0.1", port: Int = 8080) {
 	private val socket = Socket(host, port)
 	private var connected = true
@@ -31,6 +19,9 @@ class Client(username: String, host: String = "127.0.0.1", port: Int = 8080) {
 
 	private val reader = Scanner(socket.getInputStream(), StandardCharsets.UTF_8.name())
 
+	/**
+	 * Starts the client and listens for input
+	 */
 	fun run() {
 		thread { receive() }
 
@@ -43,6 +34,11 @@ class Client(username: String, host: String = "127.0.0.1", port: Int = 8080) {
 		}
 	}
 
+	/**
+	 * Sends a message to the server
+	 * @param message The message to send
+	 * @throws IOException If the connection is closed
+	 */
 	private fun send(message: String) {
 		if (!connected) {
 			throw IOException("Connection is closed")
@@ -51,7 +47,8 @@ class Client(username: String, host: String = "127.0.0.1", port: Int = 8080) {
 	}
 
 	/**
-	 * Checks for new messages in a polling-style
+	 * Checks for new messages via a blocking call.
+	 * If the connection is lost, it will print an error and set connected to false
 	 */
 	private fun receive() {
 		while (connected) {

src/main/kotlin/Util.kt
@@ -1,11 +1,15 @@
 import java.net.Socket
 import java.nio.charset.StandardCharsets
 
+/**
+ * Sends a message to a socket
+ * @param client The socket to send the message to
+ * @param message The message to send
+ */
 fun sendMessageToSocket(client: Socket, message: String) {
 	val writer = client.getOutputStream()
 	// Explicit encoding into UTF-8
 	writer.write((message + '\n').toByteArray(StandardCharsets.UTF_8))
 }
-
 
 const val ID_DISCONNECT = "#DISCONNECT"

commit 602d10221201812461bff6e9473871a6e11da497

Fix OutOfMemoryError
Issue was introduced in 59d210b36ad4962b6b05a6489b871a62a6130636

src/main/kotlin/Server.kt
@@ -30,8 +30,9 @@ class Server(private val port: Int) {
 	fun listen() {
 		Logger.log("Server listening on port $port")
 		while (true) {
+			val client = server.accept()
 			thread {
-				ClientHandler(server.accept()).run()
+				ClientHandler(client).run()
 			}
 		}
 	}